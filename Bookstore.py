9ea51e5a255f1c09b98d1da3b81688b66b2add4f
2020-year-12-classwork-mmc8158 <mmc8158@stacmail.net>
2020-02-21 15:04:16 +1300

    Create Bookstore.py

---
+++ Bookstore.py
@@ -0,0 +1,15 @@
+
+
+
+running=True
+
+while running==True:
+    print ("hi welcome to my bookshop!")
+    print ("1  add a book title.")
+    print ("2  add an existing book title.")
+    print ("3  delete a book title.")
+    print ("4  end the programme.")
+
+    command=input ("please enter 1, 2, 3, or bye")
+    if command==("1"):
+        print ("please enter what book title you want to add")
